99f2384cadd334a9bb2de8a6fefc66f9bda1f2cc
Tim Smart <tim.smart@arisechurch.com>
2021-05-07 15:32:48 +1200

    Export rest client

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,4 @@
 export * as GatewayClient from "./gateway/client";
 export * as GatewayCommands from "./gateway/commands";
+export * as RestClient from "./rest/client";
 export * as Client from "./client";

7618e0e0dfc1696f45ee4ead3ba8839dc813e333
Tim Smart <tim.smart@arisechurch.com>
2021-06-18 20:27:48 +1200

    Document flags functions

--- packages/droff-helpers/src/flags.ts
+++ packages/droff-helpers/src/flags.ts
@@ -1,11 +1,17 @@
 export type Flags<T extends number | bigint> = Record<string, T>;
 
+/**
+ * Returns all the flags OR'ed together.
+ */
 export function all(flags: Flags<number>): number;
 export function all(flags: Flags<bigint>): bigint;
 export function all(flags: Flags<any>): any {
   return Object.values(flags).reduce((acc, flag) => acc | flag);
 }
 
+/**
+ * Returns a function that converts a bitfield to a list of flag names.
+ */
 export function toList<T extends Flags<number>>(
   flags: T,
 ): (bitfield: number) => (keyof T)[];
@@ -23,16 +29,25 @@ export function toList<T extends Flags<any>>(
     );
 }
 
+/**
+ * Returns a function that converts a list of flags names to a bigint bitfield.
+ */
 export const fromListBigint =
   <T extends Flags<bigint>>(flags: T) =>
   (list: (keyof T)[]) =>
     list.reduce((acc, key) => acc | flags[key], BigInt(0));
 
+/**
+ * Returns a function that converts a list of flags names to a bitfield.
+ */
 export const fromList =
   <T extends Flags<number>>(flags: T) =>
   (list: (keyof T)[]) =>
     list.reduce((acc, key) => acc | flags[key], 0);
 
+/**
+ * Checks if a bigint bitfield contains and a flag value.
+ */
 export const hasBigInt = (flag: bigint | string) => {
   const flagBigInt = BigInt(flag);
   return (bits: bigint | string) => {
@@ -41,6 +56,9 @@ export const hasBigInt = (flag: bigint | string) => {
   };
 };
 
+/**
+ * Checks if a bitfield contains and a flag value.
+ */
 export const has = (flag: number | string) => {
   const flagNumber = +flag;
   return (bits: number | string) => {

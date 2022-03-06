6a673aa4b2275f1165b869be40606ba0bd0ed14b
Tim Smart <tim.smart@arisechurch.com>
2022-03-07 12:55:59 +1300

    chore: remove unused import

--- packages/droff/src/gateway/client.ts
+++ packages/droff/src/gateway/client.ts
@@ -1,5 +1,4 @@
 import { identity } from "fp-ts/lib/function";
-import * as O from "fp-ts/lib/Option";
 import * as Rx from "rxjs";
 import * as RxO from "rxjs/operators";
 import * as RL from "../rate-limits/rxjs";

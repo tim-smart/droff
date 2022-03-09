41c2f12188ad60bde7fce35a19c8cae39c1ba88d
Tim Smart <tim.smart@arisechurch.com>
2022-03-09 23:13:53 +1300

    feat: add cache hekpers

---
+++ packages/droff-helpers/src/cache.ts
@@ -0,0 +1,17 @@
+import { WatchOp } from "droff/dist/caches/resources";
+
+export const filterWatch =
+  <T>(f: (resource: T) => boolean) =>
+  (op: WatchOp<T>): WatchOp<T> =>
+    op.event === "create" || op.event === "update"
+      ? f(op.resource)
+        ? op
+        : { ...op, event: "delete" }
+      : op;
+
+export const mapWatch =
+  <T, R>(f: (resource: T) => R) =>
+  (op: WatchOp<T>): WatchOp<R> =>
+    op.event === "create" || op.event === "update"
+      ? { ...op, resource: f(op.resource) }
+      : op;

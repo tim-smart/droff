e88fc3de012fce6d468f41024ca07065caa752bf
Tim Smart <tim.smart@arisechurch.com>
2022-03-25 13:24:44 +1300

    chore: update api types

--- packages/droff/src/types.ts
+++ packages/droff/src/types.ts
@@ -4149,6 +4149,68 @@ export interface ListScheduledEventsForGuildParams {
   /** include number of users subscribed to each event */
   with_user_count?: boolean;
 }
+export interface Locale {
+  /** Danish */
+  da?: string;
+  /** German */
+  de?: string;
+  /** English, */
+  "en-GB"?: string;
+  /** English, */
+  "en-US"?: string;
+  /** Spanish */
+  "es-ES"?: string;
+  /** French */
+  fr?: string;
+  /** Croatian */
+  hr?: string;
+  /** Italian */
+  it?: string;
+  /** Lithuanian */
+  lt?: string;
+  /** Hungarian */
+  hu?: string;
+  /** Dutch */
+  nl?: string;
+  /** Norwegian */
+  no?: string;
+  /** Polish */
+  pl?: string;
+  /** Portuguese, Brazilian */
+  "pt-BR"?: string;
+  /** Romanian, Romania */
+  ro?: string;
+  /** Finnish */
+  fi?: string;
+  /** Swedish */
+  "sv-SE"?: string;
+  /** Vietnamese */
+  vi?: string;
+  /** Turkish */
+  tr?: string;
+  /** Czech */
+  cs?: string;
+  /** Greek */
+  el?: string;
+  /** Bulgarian */
+  bg?: string;
+  /** Russian */
+  ru?: string;
+  /** Ukrainian */
+  uk?: string;
+  /** Hindi */
+  hi?: string;
+  /** Thai */
+  th?: string;
+  /** Chinese, China */
+  "zh-CN"?: string;
+  /** Japanese */
+  ja?: string;
+  /** Chinese, Taiwan */
+  "zh-TW"?: string;
+  /** Korean */
+  ko?: string;
+}
 export enum MembershipState {
   INVITED = 1,
   ACCEPTED = 2,

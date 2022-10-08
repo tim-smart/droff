54fec3b446cf1ea3b9e4a923c6af70148c3d91d1
Tim Smart <tim.smart@arisechurch.com>
2022-10-09 09:12:19 +1300

    fix(droff): fallback to standard retry-after header

--- packages/droff-interactions/example/context-menu.ts
+++ packages/droff-interactions/example/context-menu.ts
@@ -4,12 +4,15 @@ import { createClient } from "droff";
 import {
   ApplicationCommandType,
   InteractionCallbackType,
+  InteractionType,
   MessageFlag,
 } from "droff/types";
 import { pipe } from "fp-ts/lib/function";
 import * as Rx from "rxjs";
 import * as RxO from "rxjs/operators";
 import * as Interactions from "../src/mod";
+import { UI, Ix } from "../../droff-helpers/src/mod";
+import { toUndefined } from "fp-ts/lib/Option";
 
 const client = createClient({
   token: process.env.DISCORD_BOT_TOKEN!,
@@ -24,13 +27,33 @@ const echo$ = pipe(
     name: "Echo",
     description: "",
   }),
-  RxO.flatMap(({ targetMessage, respond }) =>
-    respond(InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE)({
-      content: targetMessage!.content,
-      flags: MessageFlag.EPHEMERAL,
+  RxO.flatMap(({ respond }) =>
+    respond(InteractionCallbackType.MODAL)({
+      custom_id: "echo-modal",
+      title: "Echo",
+      components: UI.singleColumn([
+        UI.textInput({
+          custom_id: "text",
+          label: "Text",
+        }),
+      ]),
     }),
   ),
 );
+
+const getText = Ix.componentValue("text");
+
+I.interaction(InteractionType.MODAL_SUBMIT)
+  .pipe(
+    Interactions.filterByCustomId("echo-modal"),
+    RxO.flatMap(({ respond, interaction }) =>
+      respond(4)({
+        content: toUndefined(getText(interaction)),
+        flags: MessageFlag.EPHEMERAL,
+      }),
+    ),
+  )
+  .subscribe();
 
 // Subscribe
 Rx.merge(client.effects$, I.sync$, echo$).subscribe();
